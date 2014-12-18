c13a01e4d763141000b0acd273b5a45b708b5503
Steve Farrell <sfarrell@cern.ch>
2014-12-18 04:38:26 +0100

    added EL algorithm

---
+++ PyToolTests/MyELAlg.h
@@ -0,0 +1,47 @@
+#ifndef PYTOOLTESTS_MYELALG_H
+#define PYTOOLTESTS_MYELALG_H
+
+// Infrastructure includes
+#include "xAODRootAccess/Init.h"
+#include "xAODRootAccess/TEvent.h"
+#include "EventLoop/Algorithm.h"
+
+class JetCleaningTool;
+
+class MyELAlg : public EL::Algorithm
+{
+
+  public:
+
+    // Standard constructor
+    MyELAlg();
+
+    // Algorithm methods
+    virtual EL::StatusCode setupJob(EL::Job& job);
+    virtual EL::StatusCode fileExecute();
+    virtual EL::StatusCode histInitialize();
+    virtual EL::StatusCode changeInput(bool firstFile);
+    virtual EL::StatusCode initialize();
+    virtual EL::StatusCode execute();
+    virtual EL::StatusCode postExecute();
+    virtual EL::StatusCode finalize();
+    virtual EL::StatusCode histFinalize();
+
+
+    // Public configuration variables
+    float lepPtMin;
+    float jetPtMin;
+
+    ClassDef(MyELAlg, 1);
+
+  protected:
+
+    xAOD::TEvent* m_event; //!
+
+    #ifndef __CINT__
+    JetCleaningTool* m_jetCleaning; //!
+    #endif
+
+}; // class MyELAlg
+
+#endif
