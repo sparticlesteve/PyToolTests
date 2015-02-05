c2e11311e0fb7d293ad44f450bfeff94e0098350
Steve Farrell <sfarrell@cern.ch>
2015-02-05 01:06:10 +0100

    update reflex dictionary

--- PyToolTests/PyToolTestsDict.h
+++ PyToolTests/PyToolTestsDict.h
@@ -1,6 +1,8 @@
 #ifndef PYTOOLTESTS_PYTOOLTESTSDICT_H
 #define PYTOOLTESTS_PYTOOLTESTSDICT_H
 
+#include "AsgTools/AsgMessaging.h"
+#include "AsgTools/PropertyMgr.h"
 #include "PyToolTests/ObjWrapper.h"
 
 #endif
