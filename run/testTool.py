6b2c848370d704bebb3a9ccd687b2fd576d484f2
Steve Farrell <sfarrell@cern.ch>
2015-02-05 01:08:31 +0100

    minor

--- run/testTool.py
+++ run/testTool.py
@@ -1,12 +1,22 @@
 #!/bin/env python
 
 # Just a quick reflex dictionary test
+
+from ROOT import gROOT
+gROOT.SetBatch()
 import PyCintex
 import ROOT
-from ROOT import gROOT
 gROOT.ProcessLine('.x $ROOTCOREDIR/scripts/load_packages.C')
 
 from ROOT import JetCleaningTool
 
 # Configure the JetCleaningTool
-jetCleaning = ROOT.JetCleaningTool('JetCleaningTool')
+jetCleaning = JetCleaningTool('JetCleaningTool')
+jetCleaning.setProperty('OutputLevel', 2)
+jetCleaning.initialize()
+
+pmgr = jetCleaning.getPropertyMgr()
+for (x, y) in pmgr.getProperties():
+    print x, y
+
+
